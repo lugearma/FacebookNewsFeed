a58d9d6d07e6766f2d038f36a6249985ab2960da
Luis Arias <lugearma@gmail.com>
2016-09-10 03:06:15 -0500

    Added custom cell

--- FacebookNewsFeed/AppDelegate.swift
+++ FacebookNewsFeed/AppDelegate.swift
@@ -13,22 +13,22 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         
         window = UIWindow(frame: UIScreen.mainScreen().bounds)
         window?.makeKeyAndVisible()
         
-        let feedController = FeedController(collectionViewLayout: UICollectionViewLayout())
+        let flowLayout = UICollectionViewFlowLayout()
+        let feedController = FeedController(collectionViewLayout: flowLayout)
         let navigationViewController = UINavigationController(rootViewController: feedController)
         
         window?.rootViewController = navigationViewController
-        
+
         UINavigationBar.appearance().barTintColor = UIColor(red: 51/255, green: 90/255, blue: 149/255, alpha: 1.0)
         UINavigationBar.appearance().titleTextAttributes = [NSForegroundColorAttributeName: UIColor.whiteColor()]
         
         application.statusBarStyle = .LightContent
-        
+
         return true
     }
 
--- FacebookNewsFeed/ViewController.swift
+++ FacebookNewsFeed/ViewController.swift
@@ -8,14 +8,48 @@
 
 import UIKit
 
+let cellId = "cellId"
+
 class FeedController: UICollectionViewController {
+    
+    var items = ["Item1", "Item1", "Item1"]
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
         self.navigationItem.title = "Facebook Feed"
-        
+
         collectionView?.backgroundColor = UIColor(white: 0.95, alpha: 1.0)
+        collectionView?.registerClass(FeedCell.self, forCellWithReuseIdentifier: cellId)
+    }
+    
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return items.count
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(cellId, forIndexPath: indexPath) as! FeedCell
+        
+        
+        return cell
+    }
+}
+
+class FeedCell: UICollectionViewCell {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        
+        setupViews()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func setupViews() {
+        backgroundColor = UIColor.whiteColor()
     }
 }
 
